d4fa13f008a9733473f97da594020b81c8cb6830
Pakato14 <danieloliveira14@gmail.com>
2018-01-04 15:18:53 -0300

    Fazendo ajuste no sistema

--- src/Programa/Programa.java
+++ src/Programa/Programa.java
@@ -17,20 +17,24 @@ public class Programa {
 		for(;;) {
 			do {
 		System.out.println("Escolha abaixo que tipo de área deseja calcular:");
-		System.out.println(" 1 - Calcular área de um circulo");
-		System.out.println(" 2 - Calcular área de um quadrado");
-		System.out.println(" 3 - Calcular área de um retângulo");
-		System.out.println(" 4 - Calcular área de um trapézio");
-		System.out.println(" 5 - Calcular área de um triangulo");
+		System.out.println("1 - Calcular área de um circulo");
+		System.out.println("2 - Calcular área de um quadrado");
+		System.out.println("3 - Calcular área de um retângulo");
+		System.out.println("4 - Calcular área de um trapézio");
+		System.out.println("5 - Calcular área de um triangulo");
 		System.out.println("Pressione q para encerrar.");
 		menu = (char) System.in.read();
 		do {
-			fim = (char) System.in.read();
+			fim = (char) System.in.read();			
 		}while(fim != '\n');
 			}while(menu <'1'|menu>'7' & menu != 'q');
 
-			if(menu == 'q') break;
-			System.out.println("\n");
+			if(menu == 'q') {
+				System.out.println("O Programa foi encerrado!");
+				break;
+			}
+			System.out.print("\n");
+			
 		
 		//usar um switch case para informar os valores  e calcular área
 		switch(menu) {
@@ -39,8 +43,10 @@ public class Programa {
 			System.out.println("Insira o valor do raio do círculo: ");
 			n1 = inputData.nextInt();			
 			AreaCalculavel c = new Circulo(n1);
+			System.out.print("\n");
 			System.out.println("A área do circulo é: "+c.calculaArea());
-			System.out.println("\n");
+			System.out.print("\n");
+			System.out.println("---------------------------------------------------------------");
 			}catch (InputMismatchException erro1) {
 				System.err.println("Não é permitido inserir letras, informe apenas números inteiros!");
 				inputData.nextLine();
@@ -52,7 +58,8 @@ public class Programa {
 			n1 = inputData.nextInt();
 			AreaCalculavel q = new Quadrado(n1);
 			System.out.println("A área do quadrado é: "+q.calculaArea());
-			System.out.println("\n");
+			System.out.print("\n");
+			System.out.println("---------------------------------------------------------------");
 			}catch (InputMismatchException erro1) {
 				System.err.println("Não é permitido inserir letras, informe apenas números inteiros!");
 				inputData.nextLine();
@@ -66,7 +73,8 @@ public class Programa {
 			n2 = inputData.nextInt();
 			AreaCalculavel r = new Retangulo(n1,n2);
 			System.out.println("A área do retângulo é: "+r.calculaArea());
-			System.out.println("\n");
+			System.out.print("\n");
+			System.out.println("---------------------------------------------------------------");
 			}catch (InputMismatchException erro1) {
 				System.err.println("Não é permitido inserir letras, informe apenas números inteiros!");
 				inputData.nextLine();
@@ -82,7 +90,8 @@ public class Programa {
 			n3 = inputData.nextInt();
 			AreaCalculavel tp = new Trapezio(n1, n2, n3);
 			System.out.println("A área do trapézio é: "+tp.calculaArea());
-			System.out.println("\n");
+			System.out.print("\n");
+			System.out.println("---------------------------------------------------------------");
 			}catch (InputMismatchException erro1) {
 				System.err.println("Não é permitido inserir letras, informe apenas números inteiros!");
 				inputData.nextLine();
@@ -101,11 +110,13 @@ public class Programa {
 				tr.calculaAltura();
 				System.out.println("A altura do tringulo é: "+tr.getAltura());
 				System.out.println("O triangulo existe e possui uma área de: "+tr.calculaArea());
-				System.out.println("\n");
+				System.out.print("\n");
+				System.out.println("---------------------------------------------------------------");
 				
 			}else {
 				System.out.println("Não é possível construir um triangulo com os lados informados!");
-				System.out.println("\n");
+				System.out.print("\n");
+				System.out.println("---------------------------------------------------------------");
 			}
 			}catch (InputMismatchException erro1) {
 				System.err.println("Não é permitido inserir letras, informe apenas números inteiros!");
